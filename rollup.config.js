8248b2a344fadbf701c4a41f79d52e871a58314b
Abraham Raji <32333507+avronr@users.noreply.github.com>
2020-12-30 16:59:46 +0530

    Drop legacy node-resolve and commonjs plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 const packageJson = require('./package.json');
 
